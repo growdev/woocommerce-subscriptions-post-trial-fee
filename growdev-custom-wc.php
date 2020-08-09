01042c576a9a05d32abb8afb9a04d3c2d68202b1
Daniel Espinoza <daniel@growdevelopment.com>
2020-08-09 16:29:21 -0500

    First Commit

---
+++ growdev-custom-wc.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name:  GrowDev Custom WC
+ * Plugin URI:   https://growdevelopment.com/
+ * Description:  Custom WooCommerce plugin for demo
+ * Version:      0.1
+ * Author:       Grow Development
+ * Author URI:   https://growdevelopment.com/
+*/
+
